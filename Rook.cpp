56c2d8b194e50419aa07794ff3741d7aa7da0c46
Lupascu Marian <maryan_lup@yahoo.com>
2017-05-10 17:23:36 +0300

    Add files via upload

--- Rook.cpp
+++ Rook.cpp
@@ -2,11 +2,11 @@
 
 Rook::Rook () {}
 
-Rook::Rook ( std::pair < int, int > table, Color color, std::pair < int, int > texture, int repeat, int moves ) :
-    Pieces( table, color, texture, repeat, moves ) { type = Pieces::ROOK; }
-
-Rook::Rook ( int tableRow, int tableColumn, Color color, int textureRow, int textureColumn, int repeat, int moves ) :
-    Pieces(tableRow, tableColumn, color, textureRow, textureColumn, repeat, moves) { type = Pieces::ROOK; }
+/*Rook::Rook ( std::pair < int, int > table, Color color, std::pair < int, int > texture) :
+    Pieces( table, color, texture) { type = Pieces::ROOK; }
+*/
+Rook::Rook ( int tableRow, int tableColumn, Color color, int textureRow, int textureColumn) :
+    Pieces(tableRow, tableColumn, color, textureRow, textureColumn) { type = Pieces::ROOK; }
 
 bool Rook::valid ( std::pair < int, int > mPair, Pieces* const ChessPiece[8][8]) const {
     if( mPair.first >= 8 || mPair.first < 0 )
@@ -25,7 +25,6 @@ bool Rook::valid ( std::pair < int, int > mPair, Pieces* const ChessPiece[8][8])
 std::vector < std::pair < int, int > > Rook::getPossibleMoves(Pieces* const ChessPiece[8][8]) const {
     std::vector < std::pair < int, int > > mList;
     std::pair < int, int > aux;
-    bool ok = true;
 
     for(int i = 1; i < 8 && ok; i++)
     {
@@ -33,46 +32,46 @@ std::vector < std::pair < int, int > > Rook::getPossibleMoves(Pieces* const Ches
         if( valid ( aux, ChessPiece ) )
            mList.push_back( aux );
         else
-            ok = false;
-        if( ChessPiece[m_pozition.first + i][m_pozition.second] != NULL &&
-                ChessPiece[m_pozition.first + i][m_pozition.second] -> getColor() != getColor() )
-            ok = false;
+            break;
+        if( ChessPiece[m_pozition.first + i][m_pozition.second] != NULL)
+            if( ChessPiece[m_pozition.first + i][m_pozition.second] -> getColor() != getColor() )
+                break;
     }
-    ok = true;
+
     for(int i = 1; i < 8 && ok; i++)
     {
         aux = std::make_pair ( m_pozition.first - i, m_pozition.second );
         if( valid ( aux, ChessPiece ) )
            mList.push_back( aux );
         else
-            ok = false;
-        if( ChessPiece[m_pozition.first - i][m_pozition.second] != NULL &&
-                ChessPiece[m_pozition.first - i][m_pozition.second] -> getColor() != getColor() )
-            ok = false;
+            break;
+        if( ChessPiece[m_pozition.first - i][m_pozition.second] != NULL)
+            if( ChessPiece[m_pozition.first - i][m_pozition.second] -> getColor() != getColor() )
+                break;
     }
-    ok = true;
+
     for(int i = 1; i < 8 && ok; i++)
     {
         aux = std::make_pair ( m_pozition.first, m_pozition.second + i );
         if( valid ( aux, ChessPiece ) )
            mList.push_back( aux );
         else
-            ok = false;
-        if( ChessPiece[m_pozition.first][m_pozition.second + i] != NULL &&
-                ChessPiece[m_pozition.first ][m_pozition.second + i] -> getColor() != getColor() )
-            ok = false;
+            break;
+        if( ChessPiece[m_pozition.first][m_pozition.second + i] != NULL)
+            if( ChessPiece[m_pozition.first][m_pozition.second + i] -> getColor() != getColor() )
+                break;
     }
-    ok = true;
+
     for(int i = 1; i < 8 && ok; i++)
     {
         aux = std::make_pair ( m_pozition.first, m_pozition.second - i );
         if( valid ( aux, ChessPiece ) )
            mList.push_back( aux );
         else
-            ok = false;
-        if( ChessPiece[m_pozition.first][m_pozition.second - i] != NULL &&
-                ChessPiece[m_pozition.first ][m_pozition.second - i] -> getColor() != getColor() )
-            ok = false;
+            break;
+        if( ChessPiece[m_pozition.first][m_pozition.second - i] != NULL)
+            if( ChessPiece[m_pozition.first][m_pozition.second - i] -> getColor() != getColor() )
+                break;
     }
     return mList;
 }
